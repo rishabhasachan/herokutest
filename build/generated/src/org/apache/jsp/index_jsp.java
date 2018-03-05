c9724039cd679568a27324cc9f6702b1195ab89f
rishisach2 <rishisach2@gmail.com>
2018-03-05 21:36:51 +0530

    make it better

---
+++ build/generated/src/org/apache/jsp/index_jsp.java
@@ -0,0 +1,190 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write(" ");
+      org.apache.jasper.runtime.JspRuntimeLibrary.include(request, response, "header.jsp", out, false);
+      out.write("\n");
+      out.write("        \n");
+      out.write("\n");
+      out.write(" <div class=\"jumbotron\" style=\"background-image: url(i/indexbg.jpg) ;background-size:cover;\n");
+      out.write("\tbackground-repeat:no-repeat;\n");
+      out.write("\tbackground-attachment:fixed; \">\n");
+      out.write("     \n");
+      out.write("  \n");
+      out.write("     <div style=\"text-align: center;font-size:60px;padding-top: 150px; color:white;font-family:sans-serif;\">\n");
+      out.write("\t\t<h1 class='font2'>TEST YOUR SELF</h1>\n");
+      out.write("\t\t<p><em>Sharpen your skills, practice new stuff and improve yourself.</em></p>\n");
+      out.write("\t</div>\n");
+      out.write("     \n");
+      out.write("     <br/><br/>\n");
+      out.write("     \n");
+      out.write("     \n");
+      out.write("\t<div class=\"container\" style=\"text-align:center; background-image: url(i/s2.jpg);background-attachment: fixed;background-size: 100%\">\n");
+      out.write("                                    <h3>\n");
+      out.write("                                        <b id=\"b\" class=\"font4\">\n");
+      out.write("                                            <center>      \n");
+      out.write("                                                <h2> An <b>innovative</b> platform extend the e-learning capabilities of any educational\n");
+      out.write("                                                      institution by providing them a<b> real time analysis</b> on their institution health\n");
+      out.write("                                                </h2>\n");
+      out.write("                                                <br> \n");
+      out.write("                                                <a href=\"signup.jsp\"><button class=\"buttonsign\">Sign up for free</button></a>\n");
+      out.write("                                            </center>\n");
+      out.write("                                            \n");
+      out.write("                                        </b>\n");
+      out.write("                                    </h3>                             \n");
+      out.write("                                </div> \n");
+      out.write(" \n");
+      out.write(" <!--\n");
+      out.write("                    \n");
+      out.write("     \n");
+      out.write("     \n");
+      out.write("     \n");
+      out.write("     -->\n");
+      out.write("     \n");
+      out.write("     \n");
+      out.write("    \n");
+      out.write("\t\t\n");
+      out.write("         \n");
+      out.write(" </div>\n");
+      out.write("\n");
+      out.write(" \n");
+      out.write(" \n");
+      out.write(" \n");
+      out.write("\n");
+      out.write(" \n");
+      out.write(" \n");
+      out.write("\n");
+      out.write("\t<div class=\"container-fluid\" style=\"\">\t \n");
+      out.write("\t\t<div class=\"row\">\n");
+      out.write("\t\t\t<div class=\"col-sm-12\">\n");
+      out.write("\t\t\n");
+      out.write("\t\t\t\n");
+      out.write("                           \n");
+      out.write("\t\t\t\t<marquee onmouseover=\"this.stop()\" onmouseout=\"this.start()\">\n");
+      out.write("\t\t\t\t\t\t\t\t\t\t  \n");
+      out.write("\t\t\t\t<p>\n");
+      out.write("\t\t\t\t<b>\n");
+      out.write("                                    <em class=\"font5\" style=\"color:white\">\n");
+      out.write("\t\t\t\tRegistration for online exams are free of cost, start test <a href=\"signup.jsp\">now </a>.\n");
+      out.write("\t\t\t\t</em>\n");
+      out.write("\t\t\t\t</b>\n");
+      out.write("\t\t\t\t</p>\n");
+      out.write("\t\t\t\t\t\t\t\t\t\t \n");
+      out.write("\t\t\t\t</marquee> \n");
+      out.write("\t\t\n");
+      out.write("                            \n");
+      out.write("                            \n");
+      out.write("                           <!--\n");
+      out.write("                             <div class=\"row\" >\n");
+      out.write("                                          <div class=\"thumbnail col-md-4\" style=\"text-align:center;\">\n");
+      out.write("                                                 <h2>TAKE TEST</h2> <p> 10+ Subject Tests  </p>\n");
+      out.write("\t\t\t\t\t  </div >\n");
+      out.write("                                   \n");
+      out.write("\t\t\t\t\t  <div class=\"thumbnail col-md-4\" style=\"text-align:center;\">\n");
+      out.write("\t\t\t\t\t\t\n");
+      out.write("\t\t\t\t\t\t   <h2>REVIEW RESULTS</h2> <p>   </p>\n");
+      out.write("\t\t\t\t\t\n");
+      out.write("\t\t\t\t\t  </div >\n");
+      out.write("                                         \n");
+      out.write("\t\t\t\t\t   <div class=\"thumbnail col-md-4\" style=\"text-align:center;\">\n");
+      out.write("                                                 <h2>TESTS</h2> <p> 10+ Subject Tests  </p>\n");
+      out.write("\t\t\t\t\t  </div >\t\t\t\t\t \n");
+      out.write("                             </div>\t\n");
+      out.write("                            -->\n");
+      out.write("                            \n");
+      out.write("                            \n");
+      out.write("                            \n");
+      out.write("                            <center> <h3  style=\" font-family: 'Buda', cursive; color:white; \">Onlinexams is a web based interactive, independent and intelligent examination platform for students. \n");
+      out.write("                        This platform covers wide range of subject examination.\n");
+      out.write("                    </h3> </center>\n");
+      out.write("\t\t\t</div>\n");
+      out.write("\t\t</div>\n");
+      out.write("\t\t<br>\n");
+      out.write(" \n");
+      out.write("           <!--         <div class=\"well\" style=\";\">-->\n");
+      out.write("                    <h3 style=\"text-align:center;color: white\">\n");
+      out.write("                                     <center class=\"font2\" >     \n");
+      out.write("                                         <h3>Online test is an plateform for students to take  \n");
+      out.write("                                             tests online. It provieds mock test for campus placements as well as \n");
+      out.write("                                             other exam. Using Online test, you can take any test anu number of times\n");
+      out.write("                                             and see your performance metrices, Preparing for Campus Placement or any\n");
+      out.write("                                             other examination, online test provides you a number of mock tests to\n");
+      out.write("                                             <b>Engage, Analyze </b> your performance and Improve your skills!\n");
+      out.write("                                         </h3>\n");
+      out.write("                                         <br> \n");
+      out.write("                                     </center>\n");
+      out.write("                                       <center>\n");
+      out.write("                                           <h3 style=\"color:lightblue\">Education is the most powerful weapon which you can use to change the world.\n");
+      out.write("- Nelson Mandela</h3>\n");
+      out.write("                                           </center>\n");
+      out.write("                        \n");
+      out.write("                    </h3>                             \n");
+      out.write("             <!--   </div> -->\n");
+      out.write(" \n");
+      out.write(" \n");
+      out.write(" \n");
+      out.write(" \n");
+      out.write("\t\t\n");
+      out.write("\t<br/><br/><br/><br/><br/><br/>\n");
+      out.write("\t</div>\n");
+      out.write("\t<!--here container ends-->\n");
+      out.write(" ");
+      org.apache.jasper.runtime.JspRuntimeLibrary.include(request, response, "footer.jsp", out, false);
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
---
+++ web/checkans.jsp
@@ -0,0 +1,8 @@
+<%-- 
+    Document   : checkans
+    Created on : Jun 24, 2017, 2:22:05 AM
+    Author     : Rishabh Sachan
+--%>
+
+          
+  
